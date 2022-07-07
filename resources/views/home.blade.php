ec6464b0e94f4818ce9c17f86a74ad5e4a980069
Alperen Öztaş <alprnoztas@gmail.com>
2022-07-07 15:38:39 +0300

    dashboard to log screen vice versa links added.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a href="{{ route('dashboard') }}">Go to dashboard</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
-    Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index']);
+    Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
 });
