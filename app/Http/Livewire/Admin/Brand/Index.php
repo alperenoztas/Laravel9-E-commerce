1ab17846192f499ef6ae7c33700bb6b73dc3456c
Alperen Öztaş <alprnoztas@gmail.com>
2022-07-19 14:08:43 +0300

    brand edit delete

--- app/Http/Livewire/Admin/Brand/Index.php
+++ app/Http/Livewire/Admin/Brand/Index.php
@@ -12,8 +12,7 @@ class Index extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
-    public $name, $slug, $status;
-
+    public $name, $slug, $status, $brand_id;
     public function rules(){
         return [
             'name' => 'required|string',
@@ -26,6 +25,7 @@ class Index extends Component
         $this->name = '';
         $this->slug = '';
         $this->status = '';
+        $this->brand_id = '';
     }
 
     public function storeBrand(){
@@ -41,7 +41,49 @@ class Index extends Component
     }
     public function render()
     {
-        $brands = Brand::orderBy('id', 'desc')->paginate(1);
+        $brands = Brand::orderBy('id', 'desc')->paginate(5);
         return view('livewire.admin.brand.index',['brands'=>$brands])->extends('layouts.admin')->section('content');
+    }
+
+    public function closeModal(){
+        $this->resetInput();
+    }
+
+    public function openModal(){
+        $this->resetInput();
+    }
+
+    public function editBrand(int $brand_id){
+        $this->brand_id = $brand_id;
+        $brand = Brand::findOrFail($brand_id);
+        $this->name = $brand->name;
+        $this->slug = $brand->slug;
+        $this->status = $brand->status;
+        $this->dispatchBrowserEvent('open-modal');
+    }
+
+    public function updateBrand(){
+        $validatedData = $this->validate();
+        $brand = Brand::where('id',$this->brand_id)->first();
+        $brand->update([
+            'name' => $this->name,
+            'slug' => Str::slug($this->slug),
+            'status' => $this->status == true ? '1' : '0',
+        ]);
+        session()->flash('message', 'Brand Updated Successfully');
+        $this->dispatchBrowserEvent('close-modal');
+        $this->resetInput();
+    }
+
+    public function deleteBrand(int $brand_id){
+        $this->brand_id = $brand_id;
+    }
+
+    public function destroyBrand(){
+        $brand = Brand::where('id',$this->brand_id)->first();
+        $brand->delete();
+        session()->flash('message', 'Brand Deleted Successfully');
+        $this->dispatchBrowserEvent('close-modal');
+        $this->resetInput();
     }
 }
