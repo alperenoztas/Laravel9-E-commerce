ccbfc4b431f451b51de8760a94df0a268b13e02f
Alperen Öztaş <alprnoztas@gmail.com>
2022-07-22 10:37:27 +0300

    product view

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -14,7 +14,39 @@
                 </h3>
             </div>
             <div class="card-body">
-
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Category</th>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($products as $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>{{ $product->categoty_id }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->selling_price }}</td>
+                            <td>{{ $product->quantity }}</td>
+                            <td>{{ $product->status == '1' ? 'Hidden' : 'Visible' }}</td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="7">No products Available</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
