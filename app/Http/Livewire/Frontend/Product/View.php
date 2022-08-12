2390f0b17e3c0926fc270a70c8b85adc773e8c6c
Alperen Öztaş <alprnoztas@gmail.com>
2022-08-12 14:12:09 +0300

    product stock availability has done

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -6,7 +6,18 @@ use Livewire\Component;
 
 class View extends Component
 {
-    public $product,$category;
+    public $product,$category,$prodColorSelectedQuantity;
+
+
+    public function colorSelected($productColorId){
+
+        $productColor = $this->product->productColors()->where('id',$productColorId)->first();
+        $this->prodColorSelectedQuantity = $productColor->quantity;
+
+        if($this->prodColorSelectedQuantity == 0){
+            $this->prodColorSelectedQuantity = 'out of stock';
+        }
+    }
 
     public function mount($category,$product){
         $this->category = $category;
@@ -15,10 +26,12 @@ class View extends Component
 
     public function render()
     {
-        
+
         return view('livewire.frontend.product.view',[
             'category' => $this->category,
             'product' => $this->product
         ]);
     }
+
+
 }
