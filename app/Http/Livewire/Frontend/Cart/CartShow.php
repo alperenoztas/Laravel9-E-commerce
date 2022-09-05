ea7a87b0162547c1ec815bf29863f52bf9566e97
Alperen Öztaş <alprnoztas@gmail.com>
2022-09-05 14:24:21 +0300

    cart items quantity increment and decrement.

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -9,11 +9,121 @@ class CartShow extends Component
 {
     public $cart;
 
+    public function decrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+
+        if ($cartData) {
+
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()){
+
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if ($productColor->quantity > $cartData->quantity) {
+                    $cartData->decrement('quantity');
+
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Only'.$productColor->quantity.'Quantity available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+
+            }else{
+                if ($cartData->product->quantity > $cartData->quantity) {
+                    $cartData->decrement('quantity');
+
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else{
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Only'.$productColor->quantity.'Quantity available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+        }
+        else{
+
+            $this->dispatchBrowserEvent('message', [
+                'message' => 'Cart Not Found',
+                'type' => 'error',
+                'status' => 404
+            ]);
+
+        }
+    }
+
+    public function incrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+
+        if ($cartData) {
+
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()){
+
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if ($productColor->quantity > $cartData->quantity) {
+                    $cartData->increment('quantity');
+
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Only'.$productColor->quantity.'Quantity available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+
+            }else{
+                if ($cartData->product->quantity > $cartData->quantity) {
+                    $cartData->increment('quantity');
+
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else{
+                    $this->dispatchBrowserEvent('message', [
+                        'message' => 'Only'.$productColor->quantity.'Quantity available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+        }
+        else{
+
+            $this->dispatchBrowserEvent('message', [
+                'message' => 'Cart Not Found',
+                'type' => 'error',
+                'status' => 404
+            ]);
+
+        }
+    }
+
     public function render()
     {
-        $this->cart = Cart::where('user_id',auth()->user()->id)->get();
+        $this->cart = Cart::where('user_id', auth()->user()->id)->get();
 
-        return view('livewire.frontend.cart.cart-show',[
+        return view('livewire.frontend.cart.cart-show', [
             'cart' => $this->cart,
         ]);
     }
